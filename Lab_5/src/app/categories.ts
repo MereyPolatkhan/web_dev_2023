7be6b5d2f246343b39e1440deb1b4b4fc666859f
Merey Polatkhan <m_polatkhan@kbtu.kz>
2023-02-27 23:23:02 +0600

    made category component with codes

---
+++ Lab_5/src/app/categories.ts
@@ -0,0 +1,135 @@
+import {Product} from "./products";
+
+export interface Category {
+    id: number;
+    title: string;
+
+    products: Product[];
+}
+
+export let categories = [
+    {
+        id: 1,
+        title: "Phones",
+        products : [
+            {
+                id: 1,
+                name: 'Смартфон Apple iPhone 13 128Gb черный',
+                price: 373450,
+                description: 'A large phone with one of the best screens',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h49/hc0/46392662523934/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item',
+            },
+            {
+                id: 2,
+                name: 'Смартфон Apple iPhone 11 128Gb Slim Box черный',
+                price: 292759,
+                description: 'A great phone with one of the best cameras',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h17/h14/31489167425566/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item',
+            },
+            {
+                id: 3,
+                name: 'Смартфон Apple iPhone 14 Pro Max 256Gb фиолетовый',
+                price: 697100,
+                description: 'фиолетовый',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/h18/62948780834846/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+            },
+            {
+                id: 4,
+                name: 'Смартфон Apple iPhone SE 2022 128Gb черный',
+                price: 327583,
+                description: '6-ядерный Apple A15 Bionic',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6c/h22/49413363335198/apple-iphone-se-2022-64gb-cernyj-104153816-1.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-iphone-se-2022-128gb-chernyi-104153816/?c=750000000#!/item',
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Laptops",
+        products: [
+            {
+                id: 5,
+                name: 'Ноутбук Apple MacBook Air 13 MGN63 серый',
+                price: 480550,
+                description: 'общий объем накопителей: 256 ГБ',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h65/h0f/33125684084766/apple-macbook-air-2020-13-3-mgn63-seryj-100797845-1-Container.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-macbook-air-13-mgn63-seryi-100797845/?c=750000000#!/item',
+            },
+            {
+                id: 6,
+                name: 'Ноутбук Apple MacBook Air 13 MLY13 золотистый',
+                price: 637666,
+                description: 'процессор: Apple M2',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h11/h9a/52679167574046/noutbuk-apple-macbook-air-m2-8-cpu-8-gpu-256gb-starlight-105933751-1.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-macbook-air-13-mly13-zolotistyi-105933751/?c=750000000#!/item',
+            },
+
+            {
+                id: 7,
+                name: 'Ноутбук Apple MacBook Pro 16 Z14X000HQ серый',
+                price: 2835000,
+                description: 'процессор: Apple M1 Max',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h15/h96/68460098781214/apple-macbook-pro-16-2-m1-max-chip-108677248-1.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-macbook-pro-16-z14x000hq-seryi-108677248/?c=750000000#!/item',
+            },
+            {
+                id: 8,
+                name: 'Ноутбук Apple MacBook Pro 14 MKH53 Z15H0010E серый',
+                price: 2590000,
+                description: 'видеокарта: 32‑ядерный',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h26/h50/68504458002462/apple-apple-macbook-pro-14-2-m1-max-64-2tb-space-gray-z15h0010e-seryi-108697708-1.jpg',
+                link: 'https://kaspi.kz/shop/p/apple-macbook-pro-14-mkh53-z15h0010e-seryi-108697708/?c=750000000#!/item',
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "Headphones",
+        products: [
+            {
+                id: 9,
+                name: 'Наушники Beats Studio Buds черный',
+                price: 77500,
+                description: 'подключение: беспроводное',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfc/h7a/45106885099550/beats-studio-buds-cernyj-102184522-1-Container.jpg',
+                link: 'https://kaspi.kz/shop/p/beats-studio-buds-chernyi-102184522/?c=750000000#!/item',
+            },
+            {
+                id: 10,
+                name: 'Наушники Beats BeatsX Earphones синий',
+                price: 50990,
+                description: 'система активного шумоподавления',
+                rating: 5,
+                image:
+                    'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb8/hcb/31652251140126/beats-beatsx-earphones-blue-4802229-1-Container.jpg',
+                link: 'https://kaspi.kz/shop/p/beats-beatsx-earphones-sinii-4802229/?c=750000000#!/item',
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "Others",
+        products: [],
+    },
+]
