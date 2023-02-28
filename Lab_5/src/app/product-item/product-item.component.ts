5f66b2566aece5545f04ad35c48e91c8f3acdda3
Merey Polatkhan <m_polatkhan@kbtu.kz>
2023-02-28 20:23:11 +0600

    fixed bug with @Input product

--- Lab_5/src/app/product-item/product-item.component.ts
+++ Lab_5/src/app/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from "../products";
 
 @Component({
   selector: 'app-product-item',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent {
+  @Input() product : {
+    description: string | undefined;
+    id: number | undefined;
+    image: string | undefined;
+    link: string | undefined;
+    name: string | undefined;
+    price: number | undefined;
+    rating: number | undefined
+  };
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  constructor() {
+    this.product = new class implements Product {
+      // @ts-ignore
+      description: string | undefined;
+      // @ts-ignore
+      id: number | undefined;
+      // @ts-ignore
+      image: string | undefined;
+      // @ts-ignore
+      link: string | undefined;
+      // @ts-ignore
+      name: string | undefined;
+      // @ts-ignore
+      price: number | undefined;
+      // @ts-ignore
+      rating: number | undefined;
+    }
+  }
+
+  // share(product: { link: string }) {
+  //   window.open('https://telegram.me/share/url?url=' + product.link);
+  // }
+  //
+  // onNotify() {
+  //   window.alert('You will be notified when the product goes on sale');
+  // }
 
 }
