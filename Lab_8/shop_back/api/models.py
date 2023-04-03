0bdcbe251bf5f9d7c3cd14b693eb0af3f30065bf
Merey Polatkhan <m_polatkhan@kbtu.kz>
2023-04-03 22:35:47 +0600

    created models

---
+++ Lab_8/shop_back/api/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField()
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name + " " + str(self.price)
+
+    def to_json(self):
+        return {
+            "id": self.pk,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description,
+            "count": self.count,
+            "is_active": self.is_active,
+            "category_id": self.category.id,
+
+        }
+
+
+class Category(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            "id": self.pk,
+            "name": self.name,
+        }
